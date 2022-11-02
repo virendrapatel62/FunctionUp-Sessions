0730081bb0326203eb5a0c7c15211ce92ece272e
Virendra patel <patelvirendra62@gmail.com>
2022-11-02 20:19:09 +0530

    Update headers.js

--- middlewares/headers.js
+++ middlewares/headers.js
@@ -1,11 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get("/users", (request, response) => {
-  response.json({
-    message: "Success full",
-    url: request.url,
-    user: request.user,
+app.get("/users", (request, response, next) => {
+  response.append("name", "virendra");
+  response.append("header-value", "customhead");
+
+  response.append("content-type", "text/json");
+
+  if (!request.query.name)
+    return next(new Error("Please give the name in query"));
+
+  response.json({ message: "user" });
+});
+
+app.use((error, req, res, next) => {
+  console.log({ error });
+  res.json({
+    message: "something went wring",
+    error: error.message,
   });
 });
 
