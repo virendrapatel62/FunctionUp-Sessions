d3b5c39f6eab70bb73d52e81abd19fe4e0f010f0
Virendra patel <patelvirendra62@gmail.com>
2022-11-02 22:22:06 +0530

    added comments

--- middlewares/headers.js
+++ middlewares/headers.js
@@ -1,18 +1,18 @@
 const express = require("express");
 const app = express();
 
+// setting headers here
 app.get("/users", (request, response, next) => {
   response.append("name", "virendra");
   response.append("header-value", "customhead");
-
   response.append("content-type", "text/json");
-
   if (!request.query.name)
     return next(new Error("Please give the name in query"));
 
   response.json({ message: "user" });
 });
 
+// error handler
 app.use((error, req, res, next) => {
   console.log({ error });
   res.json({
