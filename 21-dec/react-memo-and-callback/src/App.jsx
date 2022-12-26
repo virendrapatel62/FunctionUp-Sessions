8cc58da693a5954d680ec0b545e2d5b1d3c473fd
Virendra patel <patelvirendra62@gmail.com>
2022-12-26 19:17:49 +0530

    added memoization

---
+++ 21-dec/react-memo-and-callback/src/App.jsx
@@ -0,0 +1,71 @@
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import { Fragment, useMemo, useState } from "react";
+import { useCallback } from "react";
+function getFactorial(number) {
+  let factorial = 1;
+
+  for (let i = 1; i <= number; i++) {
+    console.log("Calculating factorial ", i);
+    factorial = factorial * i;
+  }
+  return factorial;
+}
+
+function _Factorial({ number, onChange, factorial }) {
+  console.log(Factorial.name + " is rendring");
+  return (
+    <Fragment>
+      <input type="number" onChange={onChange} value={number} />
+      <h1>Factorial : {factorial}</h1>
+    </Fragment>
+  );
+}
+
+const Factorial = React.memo(_Factorial);
+
+function App() {
+  const [number, setNumber] = useState(3);
+  const [counter, setCounter] = useState(1);
+  const factorial = useMemo(() => {
+    return getFactorial(number);
+  }, [number]);
+
+  const handleChange = useCallback((event) => {
+    setNumber(event.target.value);
+  }, []);
+
+  // const handleChange = useMemo(() => {
+  //   console.log("MEMO FUNCTION ");
+  //   return (event) => {
+  //     setNumber(event.target.value);
+  //   };
+  // }, []);
+
+  const handleClick = (value) => {
+    const newValue = counter + value;
+
+    if (newValue < 0) {
+      return;
+    }
+    setCounter(newValue);
+  };
+
+  return (
+    <div>
+      <Factorial
+        onChange={handleChange}
+        factorial={factorial}
+        number={number}
+      ></Factorial>
+      <h1 className="counter">
+        <span onClick={() => handleClick(-1)}>-</span>
+        <span>{counter}</span>
+        <span onClick={() => handleClick(1)}>+</span>
+      </h1>
+    </div>
+  );
+}
+
+export default App;
