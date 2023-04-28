660b18e31dcfdb8e9f8dacb1c95c935d4bc8d017
Virendra patel <patelvirendra62@gmail.com>
2023-04-28 19:19:08 +0530

    added routing

--- tdemo/demo/src/App.jsx
+++ tdemo/demo/src/App.jsx
@@ -1,9 +1,18 @@
-import { useState } from "react";
+import { Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <div className="App">Home page</div>;
+  return (
+    <Fragment>
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/login" element={<h1>Login</h1>} />
+        <Route path="/register" element={<h1>Register</h1>} />
+      </Routes>
+    </Fragment>
+  );
 }
 
 export default App;
