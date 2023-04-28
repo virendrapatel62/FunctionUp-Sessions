8056969bca4d6731bf1f23246e62d25fd255f65a
Virendra patel <patelvirendra62@gmail.com>
2023-04-28 19:21:47 +0530

    createdpage

--- tdemo/demo/src/App.jsx
+++ tdemo/demo/src/App.jsx
@@ -2,14 +2,17 @@ import { Fragment, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
     <Fragment>
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
-        <Route path="/login" element={<h1>Login</h1>} />
-        <Route path="/register" element={<h1>Register</h1>} />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </Fragment>
   );
